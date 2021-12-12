1299157486a0030de9e6e916931e343814d96548
hixcoder <93520723+hixcoder@users.noreply.github.com>
2021-12-12 08:23:57 +0100

    born2beroot_v4
    here we add some good stuff to the script for make it easier to read.

--- monitoring.sh
+++ monitoring.sh
@@ -1,15 +1,15 @@
 #! /bin/sh
 
-Architecture=$(hostnamectl | grep -o 'Kernel.*\|Operating System.*\|Architecture.*' | cut -f2- -d':' | tr -d '\n' && echo)
-CPUphysical=$(cat /proc/cpuinfo | grep processor | wc -l | tr -d '\n' && echo)
-vCPU=$(cat /proc/cpuinfo | grep processor | wc -l | tr -d '\n' && echo)
-MemoryUsage=$(free -m | awk 'NR==2{printf "%s/%sMB (%.2f%%)", $3, $2, ($3*100)/$2}' | tr -d '\n'&& echo)
-DiskUsage=$(df -h | awk '$NF=="/"{printf "%s/%s (%s)", $3, $2, $5}' && echo)
-CPUload=$(top -bn1 | grep load | awk '{printf "%.1f%%", $(NF-2)}' && echo)
-Lastboot=$(who -b | awk '{printf "%s %s", $(NF-1), $NF}'&& echo)
+Architecture=$(uname -a)
+CPUphysical=$(cat /proc/cpuinfo | grep 'physical id' | wc -l)
+vCPU=$(cat /proc/cpuinfo | grep processor | wc -l)
+MemoryUsage=$(free -m | awk 'NR==2{printf "%s/%sMB (%.2f%%)\n", $3, $2, ($3 * 100) / $2}')
+DiskUsage=$(df -h | awk '$NF=="/"{printf "%s/%s (%s)\n", $3, $2, $5}')
+CPUload=$(top -bn1 | grep load | awk '{printf "%.1f%%\n", $(NF-2)}')
+Lastboot=$(who -b | awk '{printf "%s %s\n", $(NF-1), $NF}')
 LVMuse=$(if lsblk | grep -q lvm; then echo yes; else echo no; fi)
-ConnexionsTCP=$(netstat -an | grep ESTABLISHED | wc -l | tr -d '\n' && echo)
-Userlog=$(who | cut -d ' ' -f 1 | sort -u | wc -l | tr -d '\n' && echo)
+ConnexionsTCP=$(netstat -an | grep ESTABLISHED | wc -l)
+Userlog=$(who | awk '{printf "%s\n", $1}' | sort -u | wc -l)
 Network=$(hostname -I | awk '{printf "IP %s ", $NF}' | tr -d '\n' && ip link | grep 'link/ether'| awk '{printf "(%s)\n", $2}')
 Sudo=$(cat /var/log/sudo/sudo.log | grep COMMAND | wc -l)
 
@@ -32,3 +32,6 @@ wall "
 # another way for count number of sudo cmds
 #printf "#Sudo           : " && history | grep 'sudo' | awk '{printf "%s\n", $2}' | grep 'sudo' | wc -l
 #printf "#Sudo           : " && sudo grep 'sudo ' /var/log/sudo/sudo.log | wc -l | tr -d '\n' | awk '{printf "%d cmd\n", $1}'
+
+#for display architecture:
+#Architecture=$(hostnamectl | grep -o 'Kernel.*\|Operating System.*\|Architecture.*' | cut -f2- -d':' | tr -d '\n' && echo)
